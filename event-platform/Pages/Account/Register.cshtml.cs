b560696c977b9668e52302b7864655f68f161fe9
Bogdan Nikolov <84232456+bnn16@users.noreply.github.com>
2023-03-02 08:54:30 -0800

    feat: working confirm email + authentication

--- event-platform/Pages/Account/Register.cshtml.cs
+++ event-platform/Pages/Account/Register.cshtml.cs
@@ -2,6 +2,8 @@ using event_platform.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
+using System.Net.Mail;
 using System.Security.Policy;
 
 namespace event_platform.Pages.Account
@@ -34,8 +36,20 @@ namespace event_platform.Pages.Account
             if (res.Succeeded)
             {
                 var conToken = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
-                return Redirect(Url.PageLink(pageName: "/Account/ConfirmEmail", values: new { userID = user.Id, token = conToken }));
-                //return RedirectToPage("/Account/Login");
+                var conLink = Url.PageLink(pageName: "/Account/ConfirmEmail", values: new { userID = user.Id, token = conToken });
+
+                var mailMsg = new MailMessage("b.nikolov@student.fontys.nl", user.Email, "Confirm your email address", $"Please click on this link to confirm the mail: {conLink}");
+                using (var email = new SmtpClient("smtp-relay.sendinblue.com", 587)) 
+                {
+                    //change to .env file to refactor the code or something?
+                    email.Credentials = new NetworkCredential("b.nikolov@student.fontys.nl", "HyQzRFhB0jX3W6Mt");
+
+                    await email.SendMailAsync(mailMsg);
+
+                }
+
+
+                return RedirectToPage("/Account/Login");
             }
             else
             {
